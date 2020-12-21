cfa60c712dee5a784c554e7935abfe38baa2923e
Iacopo <iacopo.ghila@gmail.com>
2020-12-21 18:37:10 +0100

    Tv Shows

--- js/script.js
+++ js/script.js
@@ -4,24 +4,46 @@ const app = new Vue({
     data: {
         imgPath: 'https://image.tmdb.org/t/p/w220_and_h330_face/',
         search:'',
-        movies: []
+        shows: []
     },
     methods: {
+        flagImg(movie) {
+            if (movie.original_language == "it" || movie.original_language == "en") {
+                return ("img/" + movie.original_language.toLowerCase() + ".png");
+            } else return false;
+        },
+        movieRate(movie) {
+            return Math.ceil(movie.vote_average / 2);
+        },
         //funzione search
         searchMovie(){
-            //chiamata API
-            axios.get('https://api.themoviedb.org/3/search/movie',
-            {
-                params: {
-                    api_key: '63d036c152cd4651d8a116600d977c32',
-                    query: this.search,
-                    language: 'it-IT'
-                }
-            })
-            .then((response) => {
+            axios.all([
+                //chiamata API movie
+                axios.get('https://api.themoviedb.org/3/search/movie',
+                {
+                    params: {
+                        api_key: '63d036c152cd4651d8a116600d977c32',
+                        query: this.search,
+                        language: 'it-IT'
+                    }
+                }),
+                //chiamata API serie
+                axios.get('https://api.themoviedb.org/3/search/tv',
+                {
+                    params: {
+                        api_key: '63d036c152cd4651d8a116600d977c32',
+                        query: this.search,
+                        language: 'it-IT'
+                    }
+                })
+            ])
+            .then(axios.spread((movies, tvshows) => {
                 //salvo i risultati nell'array movies
-                this.movies = response.data.results;
-            });
+                const moviesArr = movies.data.results;
+                const tvShows = tvshows.data.results;
+
+                this.shows = moviesArr.concat(tvShows);        
+            }));
             this.search = '';
         },
         reset() {
